e40134b1fccd756ed33e25c67e066243e2f1f4a6
Alex Dmitriev <107206552+Carbofoss@users.noreply.github.com>
2023-12-29 00:13:23 +0300

    Add files via upload

---
+++ MySecondProject/MySecondProject/MySecondProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySecondProjectApp.swift
+//  MySecondProject
+//
+//  Created by MacBook on 02.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MySecondProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
